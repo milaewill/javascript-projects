d768ee0a6e84f2124ca63fdef419904bff9f5a89
Mila Williams <milaewill@Milas-MacBook-Pro.local>
2023-10-05 18:45:26 -0500

    modexercise

--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,12 @@
-function randomFromArray(arr){
-  //Your code here to select a random element from the array passed to the function.
+
+function randomFromArray(arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
 }
+//Your code here to select a random element from the array passed to the function.
+
 
 //TODO: Export the randomFromArray function.
+module.exports = {
+  randomFromArray: randomFromArray
+}
