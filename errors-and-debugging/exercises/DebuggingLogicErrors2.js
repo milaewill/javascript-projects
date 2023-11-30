542eb7f7b9176c2359a02a2139bb17de71827ac6
milaewill <142360731+milaewill@users.noreply.github.com>
2023-11-30 13:26:33 -0600

    Update DebuggingLogicErrors2.js

--- errors-and-debugging/exercises/DebuggingLogicErrors2.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors2.js
@@ -5,9 +5,10 @@
 //Given the fuelLevel value, should launchReady be true or false after the check?  Is the program behaving as expected?
 
 let launchReady = false;
+let crewReady = false;
 let fuelLevel = 17000;
-// let crewStatus = true;
-// let computerStatus = 'green';
+let crewStatus = true;
+let computerStatus = 'green';
 
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
@@ -17,17 +18,20 @@ if (fuelLevel >= 20000) {
    launchReady = false;
 }
 
-// if (crewStatus && computerStatus === 'green'){
-//    console.log('Crew & computer cleared.');
-//    launchReady = true;
-// } else {
-//    console.log('WARNING: Crew or computer not ready!');
-//    launchReady = false;
-// }
+console.log("launchReady = ", launchReady);
 
-// if (launchReady) {
-//    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
-//    console.log('Liftoff!');
-// } else {
-//    console.log('Launch scrubbed.');
-// }
+if (crewStatus && computerStatus === 'green') {
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}
+console.log("crewReady = ", crewReady);
+
+if (launchReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
